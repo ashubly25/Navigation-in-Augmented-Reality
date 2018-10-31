0f67b2c07c69326810ec96a43067c8239bef9151
ashubly25 <ashutosh2564@gmail.com>
2018-10-31 22:59:58 +0530

    Added project

---
+++ Assets/Scripts/NavController.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class NavController : MonoBehaviour {
+
+    public AStar AStar;
+    private Transform destination;
+    private bool _initialized = false;
+    private bool _initializedComplete = false;
+    private List<Node> path = new List<Node>();
+    private int currNodeIndex = 0;
+    private float maxDistance = 1.1f;
+
+    private void Start() {
+#if UNITY_EDITOR
+        InitializeNavigation();
+#endif
+    }
+
+    /// <summary>
+    /// Returns the closest node to the given position.
+    /// </summary>
+    /// <returns>The closest node.</returns>
+    /// <param name="point">Point.</param>
+    Node ReturnClosestNode(Node[] nodes, Vector3 point) {
+        float minDist = Mathf.Infinity;
+        Node closestNode = null;
+        foreach (Node node in nodes) {
+            float dist = Vector3.Distance(node.pos, point);
+            if (dist < minDist) {
+                closestNode = node;
+                minDist = dist;
+            }
+        }
+        return closestNode;
+    }
+
+    public void InitializeNavigation() {
+        StopAllCoroutines();
+        StartCoroutine(DelayNavigation());
+    }
+
+    IEnumerator DelayNavigation() {
+        while(FindObjectOfType<DiamondBehavior>() == null){
+            yield return new WaitForSeconds(.5f);
+            Debug.Log("waiting for shapes to load...");
+        }
+        InitNav();
+    }
+
+    void InitNav(){
+        if (!_initialized) {
+            _initialized = true;
+            Debug.Log("INTIALIZING NAVIGATION!!!");
+            Node[] allNodes = FindObjectsOfType<Node>();
+            Debug.Log("NODES: " + allNodes.Length);
+            Node closestNode = ReturnClosestNode(allNodes, transform.position);
+            Debug.Log("closest: " + closestNode.gameObject.name);
+            Node target = FindObjectOfType<DiamondBehavior>().GetComponent<Node>();
+            Debug.Log("target: " + target.gameObject.name);
+            //set neighbor nodes for all nodes
+            foreach (Node node in allNodes) {
+                node.FindNeighbors(maxDistance);
+            }
+
+            //get path from A* algorithm
+            path = AStar.FindPath(closestNode, target, allNodes);
+
+            if (path == null) {
+                //increase search distance for neighbors
+                maxDistance += .1f;
+                Debug.Log("Increasing search distance: " + maxDistance);
+                _initialized = false;
+                InitNav();
+                return;
+            }
+
+            //set next nodes 
+            for (int i = 0; i < path.Count - 1; i++) {
+                path[i].NextInList = path[i + 1];
+            }
+            //activate first node
+            path[0].Activate(true);
+            _initializedComplete = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (_initializedComplete && other.CompareTag("waypoint")) {
+            currNodeIndex = path.IndexOf(other.GetComponent<Node>());
+            if (currNodeIndex < path.Count - 1) {
+                path[currNodeIndex + 1].Activate(true);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class Node : MonoBehaviour {
+
+    public Vector3 pos;
+
+    [Header("A*")]
+    public List<Node> neighbors = new List<Node>();
+    public float FCost { get { return GCost + HCost; } }
+    public float HCost { get; set; }
+    public float GCost { get; set; }
+    public float Cost { get; set; }
+    public Node Parent { get; set; }
+
+    //next node in navigation list
+    public Node NextInList { get; set; }
+
+    private Vector3 scale;
+    private bool isDestination = false;
+
+    private void Awake() {
+        transform.GetChild(0).gameObject.SetActive(false);
+        //save scale
+        scale = transform.localScale;
+        //check if destination
+        if (GetComponent<DiamondBehavior>() != null) isDestination = true;
+#if UNITY_EDITOR
+        pos = transform.position;
+#endif
+    }
+
+    public void Activate(bool active) {
+        transform.GetChild(0).gameObject.SetActive(active);
+        if (NextInList != null) {
+            transform.LookAt(NextInList.transform);
+        }
+    }
+
+    void Update() {
+        //make pulsate
+        if (!isDestination)
+            transform.localScale = scale * (1 + Mathf.Sin(Mathf.PI * Time.time) * .2f); 
+    }
+
+    public void FindNeighbors(float maxDistance) {
+        foreach (Node node in FindObjectsOfType<Node>()) {
+            if (Vector3.Distance(node.pos, this.pos) < maxDistance) {
+                neighbors.Add(node);
+            }
+        }
+    }
+}
